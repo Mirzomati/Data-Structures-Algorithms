57b6476f06d353e53e127bf5c4ecaaa22b4781d5
Muzaffarzoda <muzaffarzoda@gmail.com>
2024-12-15 16:57:22 -0500

    nested loop

--- dsa-01-AlgorithmAnalysis/src/BigOSingleLoop.java
+++ dsa-01-AlgorithmAnalysis/src/BigOSingleLoop.java
@@ -1,16 +1,35 @@
 public class BigOSingleLoop {
+
     public static void main(String[] args) {
         long numberOfOperations=0;
-        int n=10000;// size of  Data
-        int m=500;
+        int n=100;// size of Data
+
         long startTime;
         long endTime;
         System.out.println("Input size n is = "+ n);
 
         // Task 1 Single Loop................... O(n)
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < n; i++) {
+            numberOfOperations+=1;
+        }
+        endTime = System.currentTimeMillis();
 
+        System.out.println("Number of operation: " + numberOfOperations + " in " + (endTime-startTime) + " milliseconds" );
+        numberOfOperations =0;
 
         // Task 2 Nested Loops...................
+        startTime = System.currentTimeMillis();
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < n; i++) {
+                numberOfOperations+=1;
+            }
+        }
+
+        endTime = System.currentTimeMillis();
+
+        System.out.println("Number of operation nested Loop: " + numberOfOperations + " in " + (endTime-startTime) + " milliseconds" );
+        numberOfOperations =0;
 
         // Task 2 End...................
 // ......................................................................................................
