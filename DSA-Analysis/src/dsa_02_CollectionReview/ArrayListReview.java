6eb58e361b91e50fee93a2de5571ba044711cab9
Muzaffarzoda <muzaffarzoda@gmail.com>
2025-01-28 13:49:25 -0500

    arraylist legacy for loop iteration

--- DSA-Analysis/src/dsa_02_CollectionReview/ArrayListReview.java
+++ DSA-Analysis/src/dsa_02_CollectionReview/ArrayListReview.java
@@ -1,15 +1,26 @@
 package dsa_02_CollectionReview;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayListReview {
 
     public static void main(String[] args) {
         // Create ArrayList and a class
+        List<Student> students = new ArrayList<>();
 
-// Add elements to ArrayList
+        // Add elements to ArrayList
+        students.add(new Student(1, "Jack"));
+        students.add(new Student(2, "John"));
+        students.add(new Student(3, "Peter"));
+        students.add(new Student(4, "Berry"));
 
         // Iteration on ArrayLists
         // 1. For Loop with get(index)
-
+        System.out.println("printing with legacy for-loop");
+        for (int i = 0; i < students.size(); i++) {
+            System.out.println(students.get(i).getName());
+        }
 
         // 2. Iterator
         // Forward Iteration
