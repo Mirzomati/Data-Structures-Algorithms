4146feb7fdf9f740974a2b4893cd05a45f4429d2
Muzaffarzoda <muzaffarzoda@gmail.com>
2025-01-28 16:04:57 -0500

    set create

---
+++ DSA-Analysis/src/dsa_02_CollectionReview/SetReview.java
@@ -0,0 +1,19 @@
+package dsa_02_CollectionReview;
+
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+
+public class SetReview {
+    public static void main(String[] args) {
+        //1. create a set
+      Set<Student> set =new LinkedHashSet<>();
+        // 2 add element
+
+
+
+    }
+
+
+}
