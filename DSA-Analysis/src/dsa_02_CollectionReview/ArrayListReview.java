43fd145e7674696b3c27c5982ca7b81f34fbec76
Muzaffarzoda <muzaffarzoda@gmail.com>
2025-01-28 13:40:27 -0500

    Arraylist review created

---
+++ DSA-Analysis/src/dsa_02_CollectionReview/ArrayListReview.java
@@ -0,0 +1,31 @@
+package dsa_02_CollectionReview;
+
+public class ArrayListReview {
+
+    public static void main(String[] args) {
+        // Create ArrayList and a class
+
+// Add elements to ArrayList
+
+        // Iteration on ArrayLists
+        // 1. For Loop with get(index)
+
+
+        // 2. Iterator
+        // Forward Iteration
+
+
+        // Backwards Iteration
+
+
+        // 3. for each loop
+
+
+        // 4. Lambda
+
+
+        // Sorting Elements in List using comparator interface
+
+        //  implementations of  Comparator
+    }
+}
