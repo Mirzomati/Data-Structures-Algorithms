3c13c9fb816b4c4c6e4ec9c9ac63bd6317464bab
Muzaffarzoda <muzaffarzoda@gmail.com>
2025-01-28 20:44:47 -0500

    set find repeating char in string

--- DSA-Analysis/src/dsa_02_CollectionReview/SetReview.java
+++ DSA-Analysis/src/dsa_02_CollectionReview/SetReview.java
@@ -1,6 +1,7 @@
 package dsa_02_CollectionReview;
 
 
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -8,12 +9,30 @@ import java.util.Set;
 public class SetReview {
     public static void main(String[] args) {
         //1. create a set
-      Set<Student> set =new LinkedHashSet<>();
+        Set<Student> set = new HashSet<>();
+
         // 2 add element
+        set.add(new Student(1, "Jack"));
+        set.add(new Student(2, "John"));
+        set.add(new Student(3, "Peter"));
+        set.add(new Student(4, "Berry"));
+        System.out.println(set.add(new Student(4, "Berry")));
+        System.out.println(set);
 
-
+        //find the first repeating char in a string
+        String str = "the goat ate the grass";
+        System.out.println(firstRepeatingChar(str));
 
     }
 
 
+
+    public static Character firstRepeatingChar(String str) {
+
+        Set<Character> chars = new HashSet<>();
+
+        for (Character ch : str.toCharArray()) if (!chars.add(ch)) return ch;
+
+        return null;
+    }
 }
