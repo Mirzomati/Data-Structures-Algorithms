8f2891df9c990fce738ea7d4a41ca0595b0b03cd
Muzaffarzoda <muzaffarzoda@gmail.com>
2025-01-31 20:20:33 -0500

    LinkedList nodes

--- Data-Structure-Algorithms/src/main/java/_03_LinkedLists/LinkedListDemo.java
+++ Data-Structure-Algorithms/src/main/java/_03_LinkedLists/LinkedListDemo.java
@@ -7,7 +7,16 @@ public class LinkedListDemo {
         Node node2 = new Node(2);
         Node node3 = new Node(3);
         Node node4 = new Node(4);
-        System.out.println(node1);
-        System.out.println("Next address is:" + node1.next);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+
+        System.out.println("node1's address is: " + node1 + " Node1's next address is " + node1.next);
+        System.out.println("node2's address is: " + node2 + " Node2's next address is " + node2.next);
+        System.out.println("node3's address is: " + node3 + " Node3's next address is " + node3.next);
+        System.out.println("node4's address is: " + node4 + " Node4's next address is " + node4.next);
+
+        System.out.println(node1.next.id+ " -id of second node");
     }
 }
