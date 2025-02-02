7eb4b0b75d9df2b83054d7df8f90b422cf64e934
Muzaffarzoda <muzaffarzoda@gmail.com>
2025-02-02 11:26:08 -0500

    adding a new node to SinglyLinkedList

--- Data-Structure-Algorithms/src/main/java/_03_LinkedLists/MySinglyLinkedList.java
+++ Data-Structure-Algorithms/src/main/java/_03_LinkedLists/MySinglyLinkedList.java
@@ -5,7 +5,23 @@ public class MySinglyLinkedList {
     Node head;
     Node tail;
     int size;
+
     boolean isEmpty() {
         return head == null;
+    }
+
+    void add(int data){
+        //create a new node object from data
+        Node node = new Node(data);
+
+        if(isEmpty()){ // if the list is empty
+            head = tail = node;
+            size++;
+        }else { // if there are elements in a list
+            tail.next = node;
+            tail = node;
+            size++;
+        }
+
     }
 }
