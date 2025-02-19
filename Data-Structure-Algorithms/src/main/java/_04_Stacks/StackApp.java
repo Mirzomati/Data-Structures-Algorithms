18118d4d3a1dd935e2d5add26244d2c38fdd744a
Muzaffarzoda <muzaffarzoda@gmail.com>
2025-02-19 13:02:26 -0500

    StackApp

--- Data-Structure-Algorithms/src/main/java/_04_Stacks/StackApp.java
+++ Data-Structure-Algorithms/src/main/java/_04_Stacks/StackApp.java
@@ -1,4 +1,20 @@
 package _04_Stacks;
 
 public class StackApp {
+    public static void main(String[] args) {
+        MyStack<Integer> stack = new MyStack<>();
+        for (int i = 0; i < 5; i++) {
+            stack.push(i);
+        }
+//        System.out.println(stack.peek());
+//        System.out.println(stack.pop());
+
+        stack.push(10);
+
+        for(int i = 0; i < 5; i++){
+            System.out.println(stack.pop());
+        }
+        System.out.println(stack.peek());
+
+    }
 }
