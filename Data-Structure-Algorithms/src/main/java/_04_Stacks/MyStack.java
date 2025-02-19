af2b0fbbc6666e29a2f047393575af355a3e6855
Muzaffarzoda <muzaffarzoda@gmail.com>
2025-02-19 12:54:44 -0500

    MyStack

---
+++ Data-Structure-Algorithms/src/main/java/_04_Stacks/MyStack.java
@@ -0,0 +1,50 @@
+package _04_Stacks;
+
+import java.util.NoSuchElementException;
+
+public class MyStack<T> {
+
+    public SNode<T> bottom = null;
+    public SNode<T> top = null;
+    public int size;
+
+    public boolean isEmpty() {
+        return bottom == null;
+    }
+
+    public void push(T item) {
+        SNode<T> node = new SNode<>(item);
+        if (isEmpty()) {
+            bottom = top = node;
+        } else {
+            top.next = node;
+            top = node;
+        }
+        size++;
+    }
+
+    public T peek() {
+        return (T) top.value;
+    }
+
+    public T pop() {
+        SNode peekNode;
+        if (isEmpty()) throw new NoSuchElementException();
+        //stack has one element
+        if (top == bottom) {
+            peekNode = top;
+            top = bottom = null;
+        } else { // more than one element
+            peekNode = top;
+            SNode prev = bottom;
+            while (prev.next != top) {
+                prev = prev.next;
+            }
+            prev.next = null;
+            top = prev;
+        }
+        size--;
+        return (T) peekNode.value;
+    }
+
+}
---
+++ Data-Structure-Algorithms/src/main/java/_04_Stacks/StackApp.java
@@ -0,0 +1,4 @@
+package _04_Stacks;
+
+public class StackApp {
+}
